dd47038246c0918b9f4c5db8d02e6090fe157dc5
Amal C P <acp070146@gmail.com>
2023-12-07 20:59:59 +0530

    app.tsx updated withredirections

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from './Components/Registration/Login';
+import { Signup } from './Components/Registration/Signup';
+import { Dashboard } from './Components/Dashboard/Dashboard';
+import { Schedule } from './Components/Schedule/Schedule';
+import { Wallet } from './Components/Wallet/Wallet';
+import { Successpage } from './Components/Successpage/Successpage';
+import { Ticket } from './Components/Ticket/Ticket';
+import { Profile } from './Components/Profile/Profile';
+import { EditProfile } from './Components/Profile/EditProfile';
+import { Bookings } from './Components/Bookings/Bookings';
+import { Report } from './Components/Report/Report';
+import { Support } from './Components/Support/Support';
+import { ContactUs } from './Components/ContactUs/ContactUs';
+import { DeleteSuccess } from './Components/Successpage/DeleteSuccess';
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
+ 
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+    {
+      path: "/schedule",
+      element: <Schedule />,
+    },
+    {
+      path: "/wallet",
+      element: <Wallet />,
+    },
+    {
+      path: "/successpage",
+      element: <Successpage />,
+    },
+    {
+      path: "/ticket",
+      element: <Ticket />,
+    },
+    {
+      path: "/bookings",
+      element: <Bookings />,
+    },
+    {
+      path: "/profile",
+      element: <Profile />,
+    },
+    {
+      path: "/editprofile",
+      element: <EditProfile />,
+    },
+    {
+      path: "/report",
+      element: <Report />,
+    },
+    {
+      path: "/support",
+      element: <Support />,
+    },
+    {
+      path: "/contactus",
+      element: <ContactUs />,
+    },
+    {
+      path: "/delete",
+      element: <DeleteSuccess />,
+    },
+  ]);
+    return <RouterProvider router={router} />;
+  }
 export default App
---
+++ src/Components/Profile/EditProfile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export const EditProfile = (props: Props) => {
+  return (
+    <div>EditProfile</div>
+  )
+}
