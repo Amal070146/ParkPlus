40cdcbd8786bb2b708a7c7105aeb794508b95cb6
Amal C P <acp070146@gmail.com>
2023-12-08 22:55:57 +0530

    ticket page update

--- src/Components/Ticket/Ticket.tsx
+++ src/Components/Ticket/Ticket.tsx
@@ -1,6 +1,7 @@
 import { HeaderNav } from "../Navbar/HeaderNav";
 import { Navbar } from "../Navbar/Navbar";
-import styles from './Ticket.module.css'
+import styles from "./Ticket.module.css";
+import qr from "./assets/qr.png";
 
 type Props = {};
 
@@ -9,7 +10,22 @@ export const Ticket = (_props: Props) => {
     <div className={styles.TicketWrapper}>
       {" "}
       <HeaderNav title="Ticket" />
-
+      <div className={styles.InnerData}>
+        <img className={styles.qrcode} src={qr} alt="" />
+        <div className={styles.Details}>
+          <h2>Details : </h2>
+          <div>
+            <p>Vehicle No: KL08AP1212</p>
+            <p>Vehicle Model:</p>
+            <p> Vehicle Owner:</p> <p>Date: </p>
+            <p>Time:</p> <p>Duration:2hrs</p>
+          </div>
+        </div>
+        <div>
+          <button>Download</button>
+          <button>Share</button>
+        </div>
+      </div>
       <Navbar />
     </div>
   );
