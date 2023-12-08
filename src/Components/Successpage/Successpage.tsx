f6cee3ade283bea0de3f7e15088d600678d07e78
Amal C P <acp070146@gmail.com>
2023-12-08 14:46:19 +0530

    success and delete page update

--- src/Components/Successpage/Successpage.tsx
+++ src/Components/Successpage/Successpage.tsx
@@ -1,15 +1,25 @@
-import styles from './Successpage.module.css'
+import styles from "./Successpage.module.css";
 import { HeaderNav } from "../Navbar/HeaderNav";
 import { Navbar } from "../Navbar/Navbar";
-type Props = {}
+import image from "./assets/successimage.png";
+type Props = {};
 
 export const Successpage = (_props: Props) => {
   return (
     <div className={styles.SuccessWrapper}>
       {" "}
       <HeaderNav title="Successpage" />
-      
+      <div className={styles.DivSection}>
+        <div className={styles.ImageWrapper}>
+          <img src={image} alt="" />
+          <div>
+            <h3>Pickup Scheduled Successfully</h3>
+            <p>An associate will get in touch with you soon.</p>
+          </div>
+        </div>
+        <button>View Ticket</button>
+      </div>
       <Navbar />
     </div>
   );
-}
+};
