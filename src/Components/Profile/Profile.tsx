96232a87961ae490ec423c8f605b928c6473e100
Amal C P <acp070146@gmail.com>
2023-12-11 11:31:27 +0530

    added redirets fiel

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,5 +1,6 @@
 import { HeaderNav } from "../Navbar/HeaderNav";
 import { Navbar } from "../Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
 import styles from "./Profile.module.css";
 import {
   Settingssvg,
@@ -14,6 +15,11 @@ import {
 type Props = {};
 
 export const Profile = (_props: Props) => {
+  const navigate = useNavigate();
+  const ButtonSubmit = (path: string) => () => {
+    navigate(path);
+  };
+
   return (
     <div className={styles.ProfileWrapper}>
       {" "}
@@ -33,42 +39,42 @@ export const Profile = (_props: Props) => {
           </div>
         </div>
         <div className={styles.ButtonWrapper}>
-          <button>
+          <button onClick={ButtonSubmit("/editprofile")}>
             <div>
               <Settingssvg />
               <h2>Edit Profile</h2>
             </div>
             <RightArrowsvg />
           </button>
-          <button>
+          <button onClick={ButtonSubmit("/addvehicle")}>
             <div>
               <Locationsvg />
               <h2>Add Vehicle</h2>
             </div>
             <RightArrowsvg />
           </button>
-          <button>
+          <button onClick={ButtonSubmit("/contactus")}>
             <div>
               <Mailsvg />
               <h2>Contact Us</h2>
             </div>
             <RightArrowsvg />
           </button>
-          <button>
+          <button onClick={ButtonSubmit("/support")}>
             <div>
               <Supportsvg />
               <h2>Support</h2>
             </div>
             <RightArrowsvg />
           </button>
-          <button>
+          <button onClick={ButtonSubmit("/delete")}>
             <div>
               <Deletesvg />
               <h2>Delete Account</h2>
             </div>
             <RightArrowsvg />
           </button>
-          <button>
+          <button onClick={ButtonSubmit("/login")}>
             <div>
               <Logoutsvg />
               <h2>LogOut</h2>
